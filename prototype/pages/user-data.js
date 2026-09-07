1b41c3f7e58543c88e42b866b7372eb1a8bf16c5
bn5sy9fjhk-cloud <bn5sy9fjhk@privaterelay.appleid.com>
2026-09-07 19:41:25 +0800

    feat: pages部门空间数据独立化

---
+++ prototype/pages/user-data.js
@@ -0,0 +1,14 @@
+/* 霍桐PDS 用户端公共数据
+ * pages所有页面共享此数据源
+ * 后续接入钉钉组织架构时只替换这里
+ */
+(function(window){
+'use strict';
+window.PDS_USER_DATA={
+  departments:[
+    {id:'rd',name:'研发部'},
+    {id:'sc',name:'生产部'},
+    {id:'xs',name:'销售部'}
+  ]
+};
+})(window);
